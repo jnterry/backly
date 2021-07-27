175193a3fe04c21eae14655f222b68fe7375f25b
Jamie Terry <jt@terry.cloud>
2021-07-27 13:08:49 +0100

    Add helper function for computing set of snapshots to retain based on a retention config per period (daily, weekly, etc)

---
+++ lib/Backly/Retention.pm
@@ -0,0 +1,158 @@
+package Backly::Retention;
+
+=head1 OVERVIEW
+
+Backly::Retenion - Utilities for managing long term retention of snapshots
+
+=cut
+
+use strict;
+use warnings;
+
+use POSIX qw(floor);
+use DateTime;
+use Data::Dumper;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(
+  compute_retained
+);
+
+# Dispatch table which maps from RETENTION_INTERVALS to a function of the type:
+# ($snapshots, $limit) => $buckets
+my %RETENTION_INTERVALS = (
+	hourly      => sub { _bucket_by_prefix(@_, 'yyyymmdd_hh') },
+	daily       => sub { _bucket_by_prefix(@_, 'yyyymmdd'   ) },
+	monthly     => sub { _bucket_by_prefix(@_, 'yyyymm'     ) },
+	yearly      => sub { _bucket_by_prefix(@_, 'yyyy'       ) },
+	weekly      => sub { _bucket_by_week(@_) },
+);
+
+=head1 METHODS
+
+=over 4
+
+=item C<compute_retained>
+
+Given a list of backup snapshots, and a retention config, returns filtered list of
+snapshots which should still be retained
+
+- snap_names - Arrayref of strings representing snapshot names to test
+- retention  - Hashref retention config mapping interval names (eg, "hourly") to the number
+               of snapshots to keep under that interval
+
+=cut
+sub compute_retained {
+	my ($snap_names, $retention) = @_;
+
+	print "Compute retention for " . Dumper($snap_names);
+	print Dumper($retention);
+
+
+	my %snapshots = ();
+	$snapshots{$_} = _parse_dt($_) foreach (@$snap_names);
+
+  my @groups = ();
+	foreach my $intervalName (keys(%$retention)) {
+		my $intervalFn = $RETENTION_INTERVALS{$intervalName};
+		die "Invalid retention interval $intervalName" unless $intervalFn;
+		push @groups, {
+			buckets => $intervalFn->(\%snapshots, $retention->{$intervalName}),
+		};
+	}
+
+	# go through group and keep oldest from each bucket, storing in hash to de-dupe when we want to
+	# keep the same snapshot due to multiple retention intervals
+	my %kept = ();
+	foreach my $group (@groups) {
+		foreach my $bucket (@{$group->{buckets}}) {
+			print "Keeping from bucket " . Dumper($bucket);
+			$kept{$bucket->[0]} = 1;
+		}
+	}
+
+	my @keptNames = keys %kept;
+	return \@keptNames;
+}
+
+# Helper which groups a set of snapshots into buckets based on common prefixes
+# of the length given by $prefix
+sub _bucket_by_prefix {
+	my ($snapshots, $limit, $prefix) = @_;
+
+	my %buckets = ();
+
+	my $prefix_length = length($prefix);
+
+	# do grouping
+  foreach my $snap_name (keys %$snapshots) {
+		my $bucket = substr $snap_name, 0, $prefix_length;
+		$buckets{$bucket} //= [];
+		push @{$buckets{$bucket}}, $snap_name;
+	}
+
+	return _post_process_buckets($limit, %buckets);
+}
+
+# Helper which buckets snapshots by each Mon-Sun week
+sub _bucket_by_week {
+	my ($snapshots, $limit) = @_;
+
+	my %buckets = ();
+
+	# do grouping
+  foreach my $snap_name (keys %$snapshots) {
+		my $snap_dt = $snapshots->{$snap_name};
+		my $dow  = $snap_dt->day_of_week();
+		my $wkId = floor($snap_dt->epoch() / (86400 * 7));
+
+		# jan 01 1970 is a thursday, if $dow is > thursday, we're in the previous logical Mon-Sun week
+		my $bucket = $dow > 4 ? $wkId - 1 : $wkId;
+
+		$buckets{$bucket} //= [];
+		push @{$buckets{$bucket}}, $snap_name;
+	}
+
+	return _post_process_buckets($limit, %buckets);
+}
+
+# Given a hash of buckets, sorts the entries into order from oldest to newest,
+# and limits to just the most recent $limit buckets
+sub _post_process_buckets {
+	my ($limit, %buckets) = @_;
+
+	# ensure each bucket is in order from oldest to newest
+	foreach my $k (keys %buckets) {
+		@{$buckets{$k}} = sort @{$buckets{$k}}
+	}
+
+	# keep only the $limit most recent buckets
+	my @results = keys %buckets;
+	@results = sort { $b cmp $a } @results;
+	@results = grep { defined $_ } @results[0..$limit-1];
+	@results = map  { $buckets{$_} } @results;
+
+	return \@results;
+}
+
+# Parses a snapshot name to get DateTime
+sub _parse_dt {
+	my ($dt_str) = @_;
+	$dt_str =~ /^(\d{4})(1[0-2]|0[0-9])(3[0-1]|[0-2][0-9])_(2[0-3]|[0-1][0-9])([0-5][0-9])/;
+	my ($year,$month,$day,$hour,$minute) = ($1,$2,$3,$4,$5);
+	die "Failed to parse snapshot datetime from '$dt_str'" unless defined $1 and defined $5;
+
+	return DateTime->new(year => $year, month => $month, day => $day, hour => $hour, minute => $minute);
+}
+
+# Formats a DateTime to get a snapshot name
+sub _format_dt {
+	my ($dt) = @_;
+	return $dt->strftime("%Y%m%d_%H%M");
+}
+
+=back
+
+=cut
+
+1;
---
+++ t/Backly/Retention.t
@@ -0,0 +1,83 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 13;
+use Test::Deep qw(cmp_set);
+use List::MoreUtils qw(uniq);
+
+require_ok('Backly::Retention');
+
+use Backly::Retention qw(compute_retained);
+
+# ------------------------------------------------------
+# - TEST: Hourly Snapshots                             -
+# ------------------------------------------------------
+{
+	my @snaps = qw(
+  	20200102_0000
+		20200101_1200
+		20200101_0600
+		20200101_0500
+		20200101_0400
+		20200101_0000
+	);
+
+  cmp_set(compute_retained(\@snaps, { hourly  => 10 }), \@snaps,             'hourly-keep-all');
+	cmp_set(compute_retained(\@snaps, { hourly  =>  5 }), [@snaps[0..4]],      'hourly-keep-recent5');
+	cmp_set(compute_retained(\@snaps, { hourly  =>  3 }), [@snaps[0..2]],      'hourly-keep-recent3');
+	cmp_set(compute_retained(\@snaps, { monthly =>  1 }), [qw(20200101_0000)], 'hourly-keep-month');
+	cmp_set(compute_retained(\@snaps, { yearly  =>  1 }), [qw(20200101_0000)], 'hourly-keep-year');
+}
+
+# ------------------------------------------------------
+# - TEST: Longer term Snapshots and multiple groups    -
+# ------------------------------------------------------
+{
+	my @snaps = qw(
+  	20201201_1000
+		20201201_0900
+		20201201_0800
+		20201201_0700
+
+		20201120_0000
+
+		20201110_0000
+
+  	20201101_1000
+		20201101_0900
+		20201101_0800
+		20201101_0700
+
+		20201001_0000
+
+		20200901_0000
+		20200801_0000
+
+		20200101_0500
+
+		20190101_0000
+		20180101_0000
+		20170101_0000
+		20160101_0000
+  );
+
+	my @hourly3  = @snaps[0..2];
+	my @weekly4  = qw(20201201_0700 20201120_0000 20201110_0000 20201101_0700);
+	my @monthly4 = qw(20201201_0700 20201101_0700 20201001_0000 20200901_0000);
+	my @yearly4  = qw(20200101_0500 20190101_0000 20180101_0000 20170101_0000);
+
+  cmp_set(compute_retained(\@snaps, { hourly   =>  3 }), \@hourly3,  'multi-keep-hourly3');
+	cmp_set(compute_retained(\@snaps, { weekly   =>  4 }), \@weekly4,  'multi-keep-weekly4');
+	cmp_set(compute_retained(\@snaps, { monthly  =>  4 }), \@monthly4, 'multi-keep-monthly4');
+	cmp_set(compute_retained(\@snaps, { yearly   =>  4 }), \@yearly4,  'multi-keep-yearly4');
+
+	cmp_set(compute_retained(\@snaps, { hourly => 3, weekly =>  4 }), [uniq(@hourly3, @weekly4)], 'multi-keep-hourly-and-weekly');
+	cmp_set(compute_retained(\@snaps, { hourly => 3, yearly =>  4 }), [uniq(@hourly3, @yearly4)], 'multi-keep-hourly-and-yearly');
+	cmp_set(
+		compute_retained(\@snaps, { hourly => 3, weekly => 4, monthly => 4, yearly => 4 }),
+		[uniq(@hourly3, @weekly4, @monthly4, @yearly4)],
+		'multi-keep-all-periods'
+	);
+}
